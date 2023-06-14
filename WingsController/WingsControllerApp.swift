abcfb218beb773bf84fd0216721df71f76c7fa82
James Addyman <james.addyman@rantmedia.com>
2023-06-14 12:10:20 +0100

    Initial Commit

---
+++ WingsController/WingsControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WingsControllerApp.swift
+//  WingsController
+//
+//  Created by James Addyman on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WingsControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
